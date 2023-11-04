7bcb85d4506268e531e596d2556deeda14313aac
Gracing47 <thomas.fosu@icloud.com>
2023-11-04 18:10:13 +0100

    main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,28 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, embeddedWallet, smartWallet, metamaskWallet, } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-
+import { ACCOUNT_FACTORY_ADDRESS } from "../constants/addresses";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "optimism-goerli";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const smartWalletConfig = {
+      factoryAddress: ACCOUNT_FACTORY_ADDRESS,
+      gasless: true, 
+    }
+
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
+        supportedWallets={[
+        smartWallet(embeddedWallet(), smartWalletConfig),
+        smartWallet(metamaskWallet(), smartWalletConfig),
+
+
+  ]}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
@@ -19,3 +30,5 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,106 +1,74 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, ChainId, embeddedWallet, smartWallet, useAddress, useConnect } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
 import { NextPage } from "next";
+import { ACCOUNT_FACTORY_ADDRESS } from "../constants/addresses";
+import React, { useState } from 'react';
+
+// Embedded Wallet Configuration
+const embeddedWalletConfig = embeddedWallet({});
+
+
+const smartWalletConfig = smartWallet(embeddedWalletConfig, {
+  factoryAddress: ACCOUNT_FACTORY_ADDRESS,
+  gasless: true, 
+});
 
 const Home: NextPage = () => {
+  const address = useAddress();
+  const connect = useConnect();
+
+  const [emailInput, setEmailInput] = useState("");
+  const [personalWalletAddress, setPersonalWalletAddress] = useState<string | undefined>("undefined");
+  const [smartWalletAddress, setSmartWalletAddress] = useState<string | undefined>("undefined");
+
+  const handleLogin = async () => {
+    try {
+      const personalWallet = await connect(embeddedWalletConfig, {
+        chainId: 420,
+        loginType: "ui_email_otp",
+        email: emailInput,
+      });
+  
+      const personalWalletAddress = await personalWallet.getAddress();
+      setPersonalWalletAddress(personalWalletAddress);
+  
+      const smartWalletInstance = await connect(smartWalletConfig, {
+        personalWallet: personalWallet,
+        chainId: 420,
+      });
+  
+      const smartWalletAddressVal = await smartWalletInstance.getAddress();
+      setSmartWalletAddress(smartWalletAddressVal);
+      setEmailInput("");
+  
+    } catch (error) {
+      console.error(error);
+    }
+  };  
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
+        {address ? (
 
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
-          </p>
-
-          <div className={styles.connect}>
-            <ConnectWallet
-              dropdownPosition={{
-                side: "bottom",
-                align: "center",
-              }}
-            />
+        <>
+            <ConnectWallet/> 
+        </>
+        ) : (
+          <div className={styles.centeredContainer}>
+            <div className={styles.centeredCard}>
+              <h1>Login</h1>
+              <p>Enter your email to login.</p>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                value={emailInput}
+                onChange={(e) => setEmailInput(e.target.value)}
+              />
+              <button onClick={handleLogin}> Login </button>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
+        )}
       </div>
     </main>
   );
